4fe4b00da8f61bcc9560603e0c73e05c3bec693e
ddn05 <dadannazril@gmail.com>
2020-12-26 12:40:09 +0700

    first unity

---
+++ Assets/gerakanPlayer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gerakanPlayer : MonoBehaviour
+{
+    public float kecepatan;
+    Rigidbody rb;
+    Animator anim;
+
+    public Transform playerPutaran;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Bergerak();
+    }
+
+    void Bergerak(){
+        float gerak = Input.GetAxis("Horizontal");
+        rb.velocity = Vector3.right * gerak * kecepatan;
+
+        anim.SetFloat("Kecepatan", Mathf.Abs(gerak), 0.1f, Time.deltaTime);
+
+        playerPutaran.localEulerAngles = new Vector3(0, gerak * 90, 0);
+    }
+
+
+}
